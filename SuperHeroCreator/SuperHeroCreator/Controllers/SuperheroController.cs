2239580e1de947b3dd430b6842e4d3501bc995f0
Ryan Kunz <ryanmkunz@gmail.com>
2019-04-25 10:39:04 -0500

    Initial commit

---
+++ SuperHeroCreator/SuperHeroCreator/Controllers/SuperheroController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroCreator.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        // GET: Bacon
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Bacon/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Bacon/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bacon/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bacon/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Bacon/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bacon/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Bacon/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperHeroCreator/SuperHeroCreator/Models/Superheros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroCreator.Models
+{
+    public class Superheros
+    {
+        [Key]
+        public int Id;
+        public string Name;
+        public string AlterEgo;
+        public string PrimaryAbility;
+        public string SecondaryAbility;
+        public string Catchphrase;
+
+
+        public Superheros()
+        {
+            
+        }
+    }
+}
